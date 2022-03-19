be19f31dbdf6c4223c0bda2ac49f91ac079e4bb7
FatimaAlmashhor <fatima.amin4official@gmail.com>
2022-03-19 07:12:55 +0300

    Init : project

---
+++ class/Database/DBConnection.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Database;
+
+use PDO;
+use PDOException;
+
+class DBConnection
+{
+    private $host       = 'mysql:host=localhost;dbname=blog-site'; //or localhost
+    private $port       = 8081;
+    private $user       = 'root';
+    private $password   = '';
+    private  $conn;
+    private $stmt;
+
+    function __construct()
+    {
+        try {
+            $this->conn  = new PDO($this->host, $this->user, $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo 'Scussesfuly connected ';
+        } catch (PDOException $e) {
+            echo 'Failed To Connected' . $e;
+        }
+    }
+    function query(string $sql): void
+    {
+        $this->stmt = $this->conn->prepare($sql);
+    }
+    function bind(string $param, string $value, int $type)
+    {
+        $this->stmt->bindParam($param, $value, $type);
+    }
+    function execute(): bool
+    {
+        if ($this->stmt->execute()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    // return object
+    function fetch()
+    {
+        return $this->stmt->fetch(PDO::FETCH_OBJ);
+    }
+
+    // return array 
+    public function fetchAll(): array
+    {
+        return $this->stmt->fetchAll(PDO::FETCH_OBJ);
+    }
+
+    public function rowCount(): int
+    {
+        return $this->stmt->rowCount();
+    }
+
+    public function __destruct()
+    { {
+            $this->db = null;
+            $this->stmt = null;
+        }
+    }
+}
---
+++ class/Post/Fetch.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Post;
+
+
+use Database\DBConnection;
+use PDO;
+
+require_once __DIR__ . "/../../vendor/autoload.php";
+
+class Fetch
+{
+    private $conn;
+    public $posts;
+    function __construct()
+    {
+        $this->conn = new DBConnection();
+        $this->fetchPosts();
+    }
+
+    // fetch all the posts
+    function fetchPosts(): void
+    {
+        $this->conn->query("SELECT `blog_id` , `blog_title` , `blog_body` FROM  blogs");
+        $this->conn->execute();
+        $this->posts =  $this->conn->fetchAll();
+    }
+}
